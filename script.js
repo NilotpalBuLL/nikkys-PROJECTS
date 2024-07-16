4f242dea278fa96b497a18b164c414d03e34ef48
NilotpalBuLL <divyaniku07@gmail.com>
2024-07-16 11:23:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.from("div h1",{
+    y:100,
+    opacity:0,
+    delay:0.2,
+    duration:0.9,
+    stagger:0.2
+
+})
+gsap.from("div p",{
+    y:100,
+    opacity:0,
+    delay:0.2,
+    duration:0.9,
+    stagger:0.2
+})
